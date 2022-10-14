4c6832972396f06145ede8d9903af2881d38f3e2
joelincity <dadonye@yahoo.com>
2022-10-14 16:52:53 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - And when I can't fall asleep, I play what I call the alphabet game
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		     (c <= 'z') {
+			       putchar(c);
+			       c++,
+			}
+	putchar('\n');
+	return (0);
+}
