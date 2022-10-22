c3056498b03380842e3e7acc2d76331708c4fcb3
joelincity <dadonye@yahoo.com>
2022-10-21 17:54:24 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,14 +6,19 @@
 
 void more_numbers(void)
 {
-	int x, y;
+	int num, a;
 
-	for (x = 0; x > 10; x++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (y = 0; y < 15; y++)
-	{
-		_putchar(y + '0');
-	}
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+			{
+				_putchar((num / 10) + '0');
+			}
+			_putchar((num % 10) + '0');
+		}
+
 		_putchar('\n');
 	}
 }
