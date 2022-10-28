bccd512828ca2e0a6b82a7c69124ffe746454bc8
joelincity <dadonye@yahoo.com>
2022-10-28 06:29:50 -0700

    updated

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -21,8 +21,10 @@ void print_buffer(char *b, int size)
 		{
 			if ((index + byte) >= size)
 				printf("  ");
+
 			else
 				printf("%02x", *(b + index + byte));
+
 			if ((index % 2) != 0 && index != 0)
 				printf(" ");
 		}
@@ -30,16 +32,18 @@ void print_buffer(char *b, int size)
 		{
 			if ((index + byte) >= size)
 				break;
+
 			else if (*(b + index + byte) >= 31 &&
-				 *(b + index + byte) <= 126)
+				*(b + index + byte) <= 126)
 				printf("%c", *(b + index + byte));
+
 			else
 				printf(".");
 		}
-		if (byte >= size)
-			continue;
-		printf("\n");
+				if (byte >= size)
+					continue;
+				printf("\n");
 	}
-	if (size <= 0)
-		printf("\n");
+			if (size <= 0)
+				printf("\n");
 }
