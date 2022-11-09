e9b0c32bcb3c965616dfad6d31acc08c1b42b59a
joelincity <dadonye@yahoo.com>
2022-11-08 19:58:53 -0800

    updated

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -24,55 +24,38 @@ char *argstostr(int ac, char **av)
 
 
 
-	
+
 	if (ac == 0 || av == NULL)
-
-
 	{
-
-
 		return (NULL);
-
 	}
-
-
-
-	
 	for (x = 0; x < ac; x++)
-
-
 	{
-
-
 		for (j = 0; av[x][j] != '\0'; j++)
-
 		{
-
-																			len += 1;
-
-
-																		}
 			len += 1;
 		}
+			len += 1;
+	}
+	str = malloc(sizeof(char) * len);
 
-		str = malloc(sizeof(char) * len);
-		for (x = 0; x < ac; x++)
+	for (x = 0; x < ac; x++)
+	{
+		for (j = 0; av[x][j] != '\0'; j++)
 		{
-			for (j = 0; av[x][j] != '\0'; j++)
-		{
-				str[v] = av[x][j];
-																				v++;
-																		}
+			str[v] = av[x][j];
+			v++;
+		}
 				str[v] = '\n';
 				v++;
 		}
 				str[v] = '\0';
-				if (str != NULL)
+		if (str != NULL)
 		{
 			return (str);
 		}
 		else
-		{
-		return (NULL);
-		}
+	{
+	return (NULL);
+	}
 }
