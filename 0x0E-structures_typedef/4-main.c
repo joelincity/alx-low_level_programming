a70b9c97431a0f59082c2bb814e07fadd679891c
joelincity <dadonye@yahoo.com>
2022-11-14 04:54:51 -0800

    updated

--- 0x0E-structures_typedef/4-main.c
+++ 0x0E-structures_typedef/4-main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "../dog.h"
+#include "dog.h"
 
 /**
  * main - check the code for ALX School students.
@@ -11,7 +11,7 @@ int main(void)
 {
 	dog_t *my_dog;
 
-	my_dog = new_dog("Django", 3.5, "Jay");
+	my_dog = new_dog("Poppy", 3.5, "Bob");
 	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
 	return (0);
 }
