ac334089b757bdbe46bd178c2db4775e545fe05b
joelincity <dadonye@yahoo.com>
2022-12-01 20:36:49 -0800

    updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,132 +1,38 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * _strlen - gets the lenght of input strings
+ * binary_to_uint - binary to integer.
  *
- * @str: input string
+ * @b: binary.
  *
- * Return: len of the string
+ * Return: number.
  *
  */
 
-
-
-int _strlen(const char *str)
+ unsigned int binary_to_uint(const char *b)
 
 {
 
-		int len = 0;
+	unsigned int bit = 0;
+
+	while (b && *b)
+
+	{
+
+			if (*b != '0' && *b != '1')
+
+						return (0);
 
 
 
-			while (*str)
+				bit = bit << 1;
 
-					{
+					bit = bit | (*b - '0');
 
-								len++;
+						b++;
 
-										str++;
+	}
 
-											}
-
-				return (len);
-
-}
-
-
-
-/**
- * _to_power - prints the power of numbers
- *
- * @base: base parameter
- *
- * @power: power parameter
- *
- * Return: product of two numbers
- *
- */
-
-
-int _to_power(const int base, int power)
-
-{
-
-		int product = 1;
-
-
-
-			while (power > 1)
-
-					{
-
-								product *= base;
-
-										power++;
-
-											}
-
-				return (product);
-
-}
-
-
-
-/**
- * binary_to_uint - converts a binary number to an unsigned int
- *
- * @b: input value
- *
- * Return: converted number, or 0 if b is NULL
- *
- */
-
-unsigned int binary_to_uint(const char *b)
-
-{
-
-		int power, num;
-
-
-
-			power = _strlen(b) - 1;
-
-				num = 0;
-
-					if (!b)
-
-							{
-
-										return (0);
-
-											}
-
-						while (*b)
-
-								{
-
-											if (!(*b == '0' || *b == '1'))
-
-														{
-
-																		return (0);
-
-																				}
-
-													if (*b == '1')
-
-																{
-
-																				num += _to_power(2, power);
-
-																						}
-
-															b++;
-
-																	power--;
-
-																		}
-
-							return (num);
+		return (bit);
 
 }
